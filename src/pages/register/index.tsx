19eb9f1fd30d62a1e2e0f410607766c4ff17f0d0
ismv00 <ismv00@icloud.com>
2025-01-02 22:26:20 -0400

    feat:ajustes tela register

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,4 +1,6 @@
+import { Link } from 'react-router-dom';
 import logoImg from '../../assets/logo.svg';
+import { FiArrowLeft } from 'react-icons/fi';
 
 export default function Register() {
   return (
@@ -6,7 +8,31 @@ export default function Register() {
       <div className="content">
         <section>
           <img src={logoImg} alt="be the hero" />
+          <h1>Cadastro</h1>
+          <p>
+            Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem
+            seus pets.
+          </p>
+          <Link className="back-link" to="/">
+            <FiArrowLeft size={16} color="#e02041" />
+            Voltar para tela inicial
+          </Link>
         </section>
+
+        <form>
+          <input type="text" placeholder="Nome do herói" required />
+          <input type="email" placeholder="E-mail" required />
+          <input type="text" placeholder="WhatsApp" required />
+
+          <div className="input-group">
+            <input type="text" placeholder="Cidade" required />
+            <input type="text" placeholder="UF" required />
+          </div>
+
+          <button className="button" type="submit">
+            Cadastrar
+          </button>
+        </form>
       </div>
     </div>
   );
