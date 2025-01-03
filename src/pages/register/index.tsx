9dac73b9e74b7065e343eced07e434d172ffa49f
ismv00 <ismv00@icloud.com>
2025-01-02 22:06:24 -0400

    feat:create a register page

---
+++ src/pages/register/index.tsx
@@ -0,0 +1,13 @@
+import logoImg from '../../assets/logo.svg';
+
+export default function Register() {
+  return (
+    <div className="register-container">
+      <div className="content">
+        <section>
+          <img src={logoImg} alt="be the hero" />
+        </section>
+      </div>
+    </div>
+  );
+}
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Logon from './pages/logon';
+import Register from './pages/register';
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Logon />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
