430ad398ad912ebbc9dbd86a3696a77b3713fb50
ismv00 <ismv00@icloud.com>
2025-01-04 23:03:19 -0400

    feat:create case page

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Logon from './pages/logon';
 import Register from './pages/register';
 import Profile from './pages/profile';
+import Case from './pages/case';
 
 export default function AppRoutes() {
   return (
@@ -11,6 +12,7 @@ export default function AppRoutes() {
         <Route path="/" element={<Logon />} />
         <Route path="/register" element={<Register />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/case" element={<Case />} />
       </Routes>
     </BrowserRouter>
   );
